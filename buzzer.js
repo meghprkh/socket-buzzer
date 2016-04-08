c9b55b10b7114e3c5e874b4671feee12ac5b8ea4
Megh Parikh <meghprkh@gmail.com>
2016-04-08 14:52:59 +0530

    Use admin entered team rather than assigning teams sequentially

--- buzzer.js
+++ buzzer.js
@@ -2,15 +2,12 @@ var jwt = require('jsonwebtoken');
 var state = require('./state');
 const config = require('./config')
 
-function getTeamById(id) {
-  return state.teams.indexOf(id);
-}
-
 module.exports = function (app, io) {
   app.post('/buzzerAuth', function (req, res) {
     var password = req.body.password;
+    var team = req.body.team;
     if (password == config.BUZZER_PASSWORD) {
-      var token = jwt.sign({}, config.PRIVATE_KEY, {
+      var token = jwt.sign({team: team}, config.PRIVATE_KEY, {
         expiresIn: '5h',
         subject: 'buzzer'
       });
@@ -20,17 +17,19 @@ module.exports = function (app, io) {
 
   io.of('/buzzer').use(function(socket, next) {
     var token = socket.request._query.token;
-    if (jwt.verify(token, config.PRIVATE_KEY, {subject: 'buzzer'}))
+    var data = jwt.verify(token, config.PRIVATE_KEY, {subject: 'buzzer'})
+    if (data) {
+      socket.team = parseInt(data.team) - 1
       next();
+    }
     else next(new Error("not authorized"))
   });
 
   io.of('/buzzer').on('connection', function(socket){
-    state.teams.push(socket.id);
-    state.scores.push(0);
+    while (state.scores.length <= socket.team) state.scores.push(0);
     io.of('/public').emit('scoreboard', state.scores)
     socket.on('buzzer', function () {
-      const team = getTeamById(socket.id)
+      const team = socket.team
       if (state.queue.indexOf(team) == -1 && state.incorrectTeams.indexOf(team) == -1) {
         state.queue.push(team);
         io.of('/admin').emit('queue', state.queue)
--- state.js
+++ state.js
@@ -3,7 +3,6 @@ var fs = require('fs');
 module.exports = {
   queue: [],
   incorrectTeams: [],
-  teams: [],
   scores: [],
   questionNumber: 1,
   currentQuestion: fs.readFileSync('./questions/1', 'utf8'),
